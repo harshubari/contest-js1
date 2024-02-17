e86b42fae6cbcf7bc82e8e8b6b3d99f315126f7a
harshu2910 <127488824+harshu2910@users.noreply.github.com>
2024-02-17 14:28:24 +0530

    1.0

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-/** @format */
-
 let arr = [
     { id: 1, name: "john", age: "18", marks: 80 },
     { id: 2, name: "jack", age: "20", marks: 85 },
@@ -7,22 +5,38 @@ let arr = [
   ];
   
   function PrintStudentswithMap() {
-    //Write your code here , just console.log
-    
+    arr.map((item) => {
+      if (item.marks > 50) {
+        console.log(item);
+      }
+      return;
+    })
   }
   
   function PrintStudentsbyForEach() {
-    //Write your code here , just console.log
+    arr.forEach((item) => {
+      if(item.marks>=50){
+        console.log(item);
+      }
+    });
   }
   
   function addData() {
-    //Write your code here, just console.log
+    let newarr = [...arr];
+    newarr.push({id:4,name:"susan",age:"20",marks:45});
+    console.log(newarr);
   }
   
   function removeFailedStudent() {
-    //Write your code here, just console.log
+    arr = arr.filter((item) => item.marks >= 50);
+    console.log(arr); 
   }
   
   function concatenateArray() {
-    //Write your code here, just console.log
+    let arr1 = [
+      { id: 10, name: "john", age: "18", marks: 80 },
+      { id: 12, name: "jack", age: "20", marks: 85 },
+      { id: 13, name: "karen", age: "19", marks: 35 }, 
+    ];
+    console.log(arr.concat(arr1));
   }
